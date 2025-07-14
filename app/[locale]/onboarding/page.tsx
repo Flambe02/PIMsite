a0a69c8b78fcd5fae995618680ddd63b78f955c1
Flambe02 <thepimentaorougecompany@gmail.com>
2025-07-14 11:32:27 +0200

    chore(i18n): scaffold /app/[locale] structure for homepage and onboarding

---
+++ app/[locale]/onboarding/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+import { useTranslations } from 'next-intl';
+
+export default function Page() {
+  const t = useTranslations('onboarding');
+  return <div>{/* TODO: contenu i18n */}</div>;
+} 
